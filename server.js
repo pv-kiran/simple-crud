415a4048db64b53ac7b8558b3ce34a8c553ffa3a
Your name here <kiranpv903@gmail.com>
2022-05-30 22:10:39 +0530

    Second commit

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const port = process.env.PORT || 3000;
 const authentication = require('./middleware/authentication')
 
 
+// extra security packages
+const helmet = require("helmet");
+var cors = require('cors');
+var xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+app.set('trust proxy', 1);
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
+
+
+
+
+
 // swagger documentation configuration
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
